234506d80909c92229441a0d0d25ac9d4f29b0ce
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-11-09 01:46:41 +0530

    adding elements to stack  implementation hands-on

---
+++ src/com/javacollections/stack/AddElementToStack.java
@@ -0,0 +1,18 @@
+package com.javacollections.stack;
+
+import java.util.*;
+
+public class AddElementToStack {
+        public static void main(String[] args) {
+
+            Stack<String> stack2 = new Stack<>();
+
+            stack2.push("Geeks");
+            stack2.push("For");
+            stack2.push("Geeks");
+
+            System.out.println(stack2);
+        }
+    }
+
+
